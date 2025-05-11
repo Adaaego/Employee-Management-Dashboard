e14f4a8ad326d866e96930b2faa81be70d9b3a03
lisa <njokuleesah@gmail.com>
2025-05-11 00:29:50 +0000

    fixed layout issues for mobile view

---
+++ data.js
@@ -0,0 +1,152 @@
+const employees = [
+    // Admins
+    { 
+      firstName: "Maxwell", 
+      lastName: "Fa", 
+      sex: "Male", 
+      accountType: "admin", 
+      email: "maxwellfa.admin@aox.com", 
+      password: "admin123" 
+    },
+    { 
+      firstName: "Sara", 
+      lastName: "Liu", 
+      sex: "Female", 
+      accountType: "admin", 
+      email: "saraliu.admin@aox.com", 
+      password: "admin123" 
+    },
+    { 
+      firstName: "James", 
+      lastName: "Doe", 
+      sex: "Male", 
+      accountType: "admin", 
+      email: "jamesd.admin@aox.com", 
+      password: "admin123" 
+    },
+    { 
+      firstName: "Emily", 
+      lastName: "Smith", 
+      sex: "Female", 
+      accountType: "admin", 
+      email: "emilysmith.admin@aox.com", 
+      password: "admin123" 
+    },
+  
+    // Users
+    { 
+      firstName: "John", 
+      lastName: "Doe", 
+      sex: "Male", 
+      accountType: "user", 
+      email: "john@aox.com", 
+      password: "user123" 
+    },
+    { 
+      firstName: "Jane", 
+      lastName: "Miller", 
+      sex: "Female", 
+      accountType: "user", 
+      email: "jane@aox.com", 
+      password: "user123" 
+    },
+    { 
+      firstName: "Lucas", 
+      lastName: "Wong", 
+      sex: "Male", 
+      accountType: "user", 
+      email: "lucas@aox.com", 
+      password: "user123" 
+    },
+    { 
+      firstName: "Olivia", 
+      lastName: "Davis", 
+      sex: "Female", 
+      accountType: "user", 
+      email: "olivia@aox.com", 
+      password: "user123" 
+    },
+    { 
+      firstName: "Liam", 
+      lastName: "Brown", 
+      sex: "Male", 
+      accountType: "user", 
+      email: "liam@aox.com", 
+      password: "user123" 
+    },
+    { 
+      firstName: "Sophia", 
+      lastName: "Wilson", 
+      sex: "Female", 
+      accountType: "user", 
+      email: "sophia@aox.com", 
+      password: "user123" 
+    },
+    { 
+      firstName: "Ethan", 
+      lastName: "Moore", 
+      sex: "Male", 
+      accountType: "user", 
+      email: "ethan@aox.com", 
+      password: "user123" 
+    },
+    { 
+      firstName: "Mia", 
+      lastName: "Taylor", 
+      sex: "Female", 
+      accountType: "user", 
+      email: "mia@aox.com", 
+      password: "user123" 
+    },
+    { 
+      firstName: "Aiden", 
+      lastName: "Anderson", 
+      sex: "Male", 
+      accountType: "user", 
+      email: "aiden@aox.com", 
+      password: "user123" 
+    },
+    { 
+      firstName: "Charlotte", 
+      lastName: "Thomas", 
+      sex: "Female", 
+      accountType: "user", 
+      email: "charlotte@aox.com", 
+      password: "user123" 
+    },
+    { 
+      firstName: "Benjamin", 
+      lastName: "Jackson", 
+      sex: "Male", 
+      accountType: "user", 
+      email: "benjamin@aox.com", 
+      password: "user123" 
+    },
+    { 
+      firstName: "Amelia", 
+      lastName: "White", 
+      sex: "Female", 
+      accountType: "user", 
+      email: "amelia@aox.com", 
+      password: "user123" 
+    },
+    { 
+      firstName: "William", 
+      lastName: "Harris", 
+      sex: "Male", 
+      accountType: "user", 
+      email: "william@aox.com", 
+      password: "user123" 
+    },
+    { 
+      firstName: "Lily", 
+      lastName: "Martin", 
+      sex: "Female", 
+      accountType: "user", 
+      email: "lily@aox.com", 
+      password: "user123" 
+    }
+  ];
+  
+  console.log(employees);
+  
